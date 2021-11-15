d89d5401f1f855da9a76b62cd946424e990abb14
VigneshC-Jfrog <62242171+VigneshC-Jfrog@users.noreply.github.com>
2021-11-15 18:45:02 +0530

    Update hello.go

--- hello.go
+++ hello.go
@@ -8,4 +8,9 @@ import (
 
 func main() {
 	fmt.Println(quote.Hello())
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+    _, err := http.Get("https://golang.org/")
+    if err != nil {
+        fmt.Println(err)
+    }
 }
